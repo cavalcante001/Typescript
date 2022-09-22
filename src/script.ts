39f20fdf48f7ed156bd6a7f1b41c1444bcce4fc1
Paulo <paulocavalcante001@outlook.com>
2022-09-22 14:35:38 -0300

    Type assertions - assistência

--- src/script.ts
+++ src/script.ts
@@ -1,21 +1,2 @@
-// type User = {
-//     nome: string,
-//     idade: number
-// }
-
-interface User {
-    nome: string,
-}
-
-interface User {
-    idade: number;
-}
-
-function resumo(usuario: User) {
-    return `Olá ${usuario.nome}, você tem ${usuario.idade} anos`;
-}
-
-resumo({
-    nome: 'Paulo',
-    idade: 23
-});
+let idadeField = document.getElementById('idade') as HTMLInputElement;
+console.log(idadeField.value);
