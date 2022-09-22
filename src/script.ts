96e428aa54d717d11796e25abe6674bf5871a59e
Paulo <paulocavalcante001@outlook.com>
2022-09-22 13:44:25 -0300

    usando tipos no retorno de uma função

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,12 @@
-function firstLetterUpperCase(name) {
+function firstLetterUpperCase(name: string): string {
     let firstLetter = name.charAt(0).toUpperCase();
     return firstLetter+name.substring(1);
 }
+
+let nome = firstLetterUpperCase('paulo'); 
+
+function somar(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+let alguma = somar(90, 15);
