3aaf29bd7eddf76c85e7114aa036dc984ece375a
Paulo <paulocavalcante001@outlook.com>
2022-09-22 14:50:21 -0300

    Tipos literais

--- src/script.ts
+++ src/script.ts
@@ -1,2 +1,27 @@
-let idadeField = document.getElementById('idade') as HTMLInputElement;
-console.log(idadeField.value);
+type Opcoes = {
+    width: number,
+    height: number
+}
+function configurar(props: Opcoes | 'auto') {
+}
+
+configurar({width: 200, height: 200});
+configurar('auto');
+configurar('automatico');
+
+// function temNome(nome: string): true | false {
+//     if (nome !== '') {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
+// function mostrarTexto(texto: string, alinhamento: 'left' | 'right' | 'center') {
+//     return `<div style="text-align: ${alinhamento}">${texto}</div>`;
+// }
+
+// mostrarTexto('Paulo', 'left');
+// mostrarTexto('Paulo', 'right');
+// mostrarTexto('Paulo', 'blabla');
